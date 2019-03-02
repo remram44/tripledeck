5b175469b76ba2abb56d14be8f5a8ca138385c3f
Remi Rampin <remirampin@gmail.com>
2019-03-02 00:44:01 -0500

    WIP

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,7 +1,48 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+extern crate serde;
+extern crate uuid;
+
+use serde::{Serialize, Deserialize};
+use uuid::Uuid;
+
+#[derive(Serialize, Deserialize)]
+pub struct List {
+    pub id: Uuid,
+    pub name: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Board {
+    pub id: Uuid,
+    pub name: String,
+    pub lists: Vec<List>,
+}
+
+pub trait Storage {
+    fn add_board(&mut self, board: &Board);
+    fn get_board(&mut self, id: &Uuid) -> Board;
+    fn add_list(&mut self, board_id: &Uuid, list: &List);
+}
+
+impl Board {
+    pub fn new<S: Storage>(storage: &mut S, name: &str) -> Board {
+        let board = Board {
+            id: Uuid::new_v4(),
+            name: name.into(),
+            lists: Vec::new(),
+        };
+        storage.add_board(&board);
+        board
+    }
+
+    pub fn get<S: Storage>(storage: &mut S, id: &Uuid) -> Board {
+        storage.get_board(id)
+    }
+
+    pub fn add_list<S: Storage>(&mut self, storage: &mut S, name: &str) {
+        let list = List {
+            id: Uuid::new_v4(),
+            name: name.into(),
+        };
+        storage.add_list(&self.id, &list);
     }
 }
--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,6 +1,19 @@
+extern crate uuid;
 extern crate wasm_bindgen;
 
+extern crate tripledeck_core;
+
+use uuid::Uuid;
 use wasm_bindgen::prelude::*;
+
+use tripledeck_core::{Board, List, Storage};
+
+#[wasm_bindgen]
+pub struct BoardWrap(tripledeck_core::Board);
+
+fn uuid2str(id: &Uuid) -> String {
+    format!("{:x}", id.to_hyphenated_ref())
+}
 
 #[wasm_bindgen]
 extern {
@@ -10,4 +23,41 @@ extern {
 #[wasm_bindgen]
 pub fn test(name: &str) {
     alert(&format!("Hello, {}", name));
+}
+
+// Storage functions provided by JavaScript
+#[wasm_bindgen]
+extern {
+    pub fn storage_add_board(board: &JsValue);
+    pub fn storage_get_board(id: &str) -> JsValue;
+    pub fn storage_add_list(board_id: &str, list: &JsValue);
+}
+
+/// Adapter for Storage trait using JavaScript code.
+struct JsStorage;
+
+impl Storage for JsStorage {
+    fn add_board(&mut self, board: &Board) {
+        storage_add_board(
+            &JsValue::from_serde(board).unwrap(),
+        );
+    }
+
+    fn get_board(&mut self, id: &Uuid) -> Board {
+        storage_get_board(
+            &uuid2str(id),
+        ).into_serde().unwrap()
+    }
+
+    fn add_list(&mut self, board_id: &Uuid, list: &List) {
+        storage_add_list(
+            &uuid2str(board_id),
+            &JsValue::from_serde(list).unwrap(),
+        );
+    }
+}
+
+#[wasm_bindgen]
+pub fn get_board(id: &str) -> BoardWrap {
+    BoardWrap(Board::get(&mut JsStorage, &Uuid::parse_str(id).expect("Invalid board ID")))
 }
