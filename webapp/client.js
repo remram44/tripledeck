adf9d5a7831c846117daa633f4121aeeba5a6702
Remi Rampin <remirampin@gmail.com>
2019-03-02 11:27:24 -0500

    Update JavaScript test

--- webapp/client.js
+++ webapp/client.js
@@ -1,5 +1,15 @@
-const js = import("./dist/tripledeck_wasm");
+const client = import("./dist/tripledeck_wasm");
 
-js.then(js => {
-    js.test("World!");
+window.storage_get_board = function(id) {
+    return {
+        id: id,
+        name: "Board " + id,
+        lists: []
+    };
+}
+
+client.then(client => {
+    client.test("World!");
+    var b = client.get_board("936DA01F9ABD4d9d80C702AF85C822A8");
+    console.log("board = ", b);
 });
