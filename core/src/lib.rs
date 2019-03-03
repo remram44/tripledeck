d3130610e00d9b4c4a0a1d7cf47816ab0fb91908
Remi Rampin <remirampin@gmail.com>
2019-03-03 18:58:51 -0500

    Add App

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -4,6 +4,9 @@ extern crate uuid;
 
 use futures::Future;
 use serde::{Serialize, Deserialize};
+use std::cell::RefCell;
+use std::collections::BTreeMap;
+use std::rc::{Rc, Weak};
 use uuid::Uuid;
 
 #[derive(Serialize, Deserialize)]
@@ -22,32 +25,15 @@ pub struct Board {
 pub trait Storage {
     type Error: 'static;
 
-    fn add_board(&mut self, board: &Board)
+    fn add_board(&self, board: &Board)
         -> Box<Future<Item=(), Error=Self::Error>>;
-    fn get_board(&mut self, id: &Uuid)
+    fn get_board(&self, id: &Uuid)
         -> Box<Future<Item=Option<Board>, Error=Self::Error>>;
-    fn add_list(&mut self, board_id: &Uuid, list: &List)
+    fn add_list(&self, board_id: &Uuid, list: &List)
         -> Box<Future<Item=(), Error=Self::Error>>;
 }
 
 impl Board {
-    pub fn new<S: Storage>(storage: &mut S, name: &str)
-        -> Box<Future<Item=Board, Error=S::Error>>
-    {
-        let board = Board {
-            id: Uuid::new_v4(),
-            name: name.into(),
-            lists: Vec::new(),
-        };
-        Box::new(storage.add_board(&board).map(|()| board))
-    }
-
-    pub fn get<S: Storage>(storage: &mut S, id: &Uuid)
-        -> Box<Future<Item=Option<Board>, Error=S::Error>>
-    {
-        storage.get_board(id)
-    }
-
     pub fn add_list<S: Storage>(&mut self, storage: &mut S, name: &str)
         -> Box<Future<Item=(), Error=S::Error>>
     {
@@ -56,5 +42,71 @@ impl Board {
             name: name.into(),
         };
         storage.add_list(&self.id, &list)
+    }
+}
+
+pub struct App<S: Storage> {
+    storage: S,
+    boards: Rc<RefCell<BTreeMap<Uuid, Weak<Board>>>>,
+}
+
+impl<S: Storage> App<S> {
+    pub fn new(storage: S) -> App<S> {
+        App {
+            storage,
+            boards: Rc::new(RefCell::new(BTreeMap::new())),
+        }
+    }
+
+    pub fn new_board(&self, name: &str)
+        -> Box<Future<Item=Rc<Board>, Error=S::Error>>
+    {
+        // Make it
+        let id = Uuid::new_v4();
+        let board = Board {
+            id: id.clone(),
+            name: name.into(),
+            lists: Vec::new(),
+        };
+        let rc = Rc::new(board);
+
+        // Add it to the cache
+        self.boards.borrow_mut().insert(id, Rc::downgrade(&rc));
+
+        // Add it to storage
+        let fut = self.storage.add_board(&*rc);
+
+        let fut = fut.map(|()| rc);
+        Box::new(fut)
+    }
+
+    pub fn get_board(&self, id: &Uuid)
+        -> Box<Future<Item=Option<Rc<Board>>, Error=S::Error>>
+    {
+        // Get from cache
+        let opt = self.boards.borrow().get(id).cloned();
+        if let Some(weak) = opt {
+            if let Some(rc) = weak.upgrade() {
+                return Box::new(futures::future::ok(Some(rc)));
+            }
+        }
+
+        // Get it from storage
+        let fut = self.storage.get_board(id);
+        // Turn into Rc
+        let fut = fut.map(|opt| opt.map(|b| Rc::new(b)));
+        // Add it to the cache
+        let id = id.clone();
+        let boards_map = self.boards.clone();
+        let fut = fut.map(move |opt| {
+            if let Some(ref rc) = opt {
+                boards_map.borrow_mut().insert(
+                    id,
+                    Rc::downgrade(rc),
+                );
+            }
+            opt
+        });
+        Box::new(fut)
     }
 }
